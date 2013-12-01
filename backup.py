7d4832c0f6d789b6dbd793b6f8f73772bf103d76
Sergey Sheyko <ssic7i@gmail.com>
2013-12-01 10:40:55 -0800

    Added py

---
+++ backup.py
@@ -0,0 +1,55 @@
+import os
+import subprocess
+import sys
+
+line_in = sys.argv
+
+filelist = line_in[1]
+dest_folder = line_in[2]
+#filelist = 'list.txt'
+f = open(filelist, 'r')
+list_files = ''
+for line in f:
+ list_files = list_files + line[0:len(line)-1] + ' '
+
+i = 0
+newlist = ''
+while i < len(list_files):
+ if list_files[i] == '\\':
+  newlist = newlist + '/'
+ else:
+  newlist = newlist + list_files[i]
+ i = i+1
+ 
+i = 0
+newdest = ''
+while i < len(dest_folder):
+ if dest_folder[i] == '\\':
+  newdest = newdest + '/'
+ else:
+  newdest = newdest + dest_folder[i]
+ i = i+1
+
+#print(newlist)
+#print(dest_folder)
+#print(newdest)
+
+PIPE = subprocess.PIPE
+
+k = subprocess.Popen('tar -c ' + newlist + '--file=archive.tar', shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
+while True:
+        s = k.stdout.readline()
+        if not s: break
+        print s
+
+k = subprocess.Popen('gzip archive.tar', shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
+while True:
+        s = k.stdout.readline()
+        if not s: break
+        print s
+		
+k = subprocess.Popen('copy archive.tar.gz ' + newdest, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
+while True:
+        s = k.stdout.readline()
+        if not s: break
+        print s
